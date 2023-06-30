16d87ea5e7a646aa1c036326267c1f7842002873
Kristain Putra <kputrapar@gmail.com>
2023-06-30 21:15:28 +0700

    build(Routes): Session middleware

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,9 +1,31 @@
 package config
 
-import "html/template"
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+)
 
 // AppConfig holds the application config
 type AppConfig struct {
 	UseCache      bool
 	TemplateCache map[string]*template.Template
+	InfoLog       *log.Logger
+	InProduction  bool
+	Session       *scs.SessionManager
+}
+
+func (a *AppConfig) InitConfig() {
+	a.InProduction = false
+	a.UseCache = false
+
+	session := scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = a.InProduction
+	a.Session = session
 }
