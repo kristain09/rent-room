d515723ffa0ff3a433ef40fa3cda688b7f5aec0d
Kristain Putra <kputrapar@gmail.com>
2023-06-30 16:59:03 +0700

    refactor(Routes): Swap pat to chi becuase needs middleware

--- cmd/web/routes/routes.go
+++ cmd/web/routes/routes.go
@@ -3,16 +3,15 @@ package routes
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
 	"github.com/kristain09/rent-room/pkg/config"
 	"github.com/kristain09/rent-room/pkg/handlers"
 )
 
 func Router(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/kristain09/rent-room
 
 go 1.20
 
-require (
-	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
-	github.com/go-chi/chi/v5 v5.0.8 // indirect
-)
+require github.com/go-chi/chi/v5 v5.0.8
